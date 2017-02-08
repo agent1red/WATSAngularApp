34ede39958d861d156cacbe7f7e38e40795b79e0
agent1red <darklen14@gmail.com>
2017-02-08 11:55:53 -0800

    Bootstrapped angular and is working now

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name watsangularAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the watsangularAppApp
+ */
+angular.module('watsangularAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
