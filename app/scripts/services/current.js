fd435534f6539c744b265a464552d533e59142f4
agent1red <darklen14@gmail.com>
2017-02-13 03:00:42 -0800

    created eather app

---
+++ app/scripts/services/current.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name watsangularAppApp.current
+ * @description
+ * # current
+ * Factory in the watsangularAppApp.
+ */
+angular.module('watsangularAppApp')
+  .factory('current', function ($resource) { 
+    // Service logic
+    // ...
+
+    // Public API here
+    return $resource('http://api.openweathermap.org/data/2.5/weather?q=:location&units =imperial&APPID=6c8912d0f4d911c85542740297789701',{},{ 
+        query: {
+            method: 'GET',
+            params: {
+                location: 'Seattle,us'
+            },
+            isArray: false
+        }
+    });
+  });
